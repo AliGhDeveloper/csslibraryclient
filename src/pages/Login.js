1d5fc688389b76f305fcc98a916112db969b35b8
Ali gh <ghasempour.2m@gmail.com>
2022-11-01 11:16:57 +0330

    fixing refresh cookie

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -19,9 +19,9 @@ export default function Login(){
             dispatch({ type : "AUTH", payload: userData.data.login});
             localStorage.setItem('first login', true);
             
-            Cookies.set('refreshtoken', userData.data.login.refreshtoken, {
-                expires : 7,
-            });
+            // Cookies.set('refreshtoken', userData.data.login.refreshtoken, {
+            //     expires : 7,
+            // });
             return navigate('/')
         }
     }, [userData])
